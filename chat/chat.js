2a3b41b702c2db20e3c764124f13136f6c3b8965
dengdengkoi <115406234+dengdengkoi@users.noreply.github.com>
2024-03-22 20:54:53 +0800

    1

--- chat/chat.js
+++ chat/chat.js
@@ -105,13 +105,6 @@ function hideMessage(messageId) {
     }
 }
 
-//删除历史记录
-delHistory.addEventListener('click', function () {
-    localStorage.removeItem('msgList');
-    localStorage.setItem('msgList', JSON.stringify([])); // 将msgList设置为空数组
-    location.reload(); // 刷新页面
-})
-
 // 设置窗口显示状态
 function togglePopup() {
     var popup = document.getElementById('settings-popup');
