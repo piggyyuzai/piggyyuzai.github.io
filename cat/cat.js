3293bdf6f9536864d72fe4d19b076178aa2d1b74
dengdengkoi <115406234+dengdengkoi@users.noreply.github.com>
2024-05-02 15:02:05 +0800

    1

--- cat/cat.js
+++ cat/cat.js
@@ -1,11 +1,93 @@
+function redBorder() {
+    var elements = document.querySelectorAll('*');
+    for (var i = 0; i < elements.length; i++) {
+        if (elements[i].style.border==='none') {
+            elements[i].style.border = '1px solid red';
+        } else {
+            elements[i].style.border = 'none';
+        }
+    }
+}
 window.onload = function() {
     cat();
+    catPosition();
 }
-var id=1;
-var mirrored = false;
-var catinfo={
+id=1;
+// let catx = 100;
+// let caty = 100;
+// catx = Math.max(-window.innerWidth / 2 + 75, Math.min(window.innerWidth / 2 - 75, catx));
+// caty = Math.max(-window.innerHeight / 2 + 75, Math.min(window.innerHeight / 2 - 75, caty));
+// function catPosition() {
+//     var cat = document.getElementById('cat');
+//     catTop = window.innerHeight / 2 - 75 + caty;
+//     catLeft = window.innerWidth / 2 - 75 + catx;
+//     cat.style.top = catTop + 'px';
+//     cat.style.left = catLeft + 'px';
+// }
+// document.addEventListener('click', function(event) {
+//     var mouseX = event.clientX; // 获取鼠标点击位置相对于浏览器窗口的水平坐标
+//     var mouseY = event.clientY; // 获取鼠标点击位置相对于浏览器窗口的垂直坐标
+//     console.log('catx:'+ catx + ', caty:'+ caty);
+//     console.log('点击位置的坐标：(' + mouseX + ', ' + mouseY + ')');
+//     catx = mouseX - window.innerWidth / 2;
+//     caty = mouseY - window.innerHeight / 2;
+//     console.log('catx:'+ catx + ', caty:'+ caty);
+//
+//     var speed = 100;
+//     var distance = Math.sqrt(catx * catx + caty * caty);
+//     var duration = distance / speed * 1000;
+//
+//     var cat = document.getElementById('cat');
+//     // Move cat to clicked position
+//     cat.style.transitionDuration = duration + "ms";
+//     catPosition();
+// });
+let catx = window.innerWidth / 2 + 0;
+let caty = window.innerHeight / 2 + 0;
+catx = Math.max(75, Math.min(window.innerWidth - 75, catx));
+caty = Math.max(75, Math.min(window.innerHeight - 75, caty));
+function catPosition() {
+    var cat = document.getElementById('cat');
+    catTop = caty - 75;
+    catLeft = catx - 75;
+    cat.style.top = catTop + 'px';
+    cat.style.left = catLeft + 'px';
+}
+document.addEventListener('click', function(event) {
+    offsetX = event.clientX - catx;
+    offsetY = event.clientY - caty;
+    a=1;
+    if(offsetX<0){
+        b=-1;
+        if(a!=b){
+            mirrorCat();
+        }
+        a=b;
+    }
+    if(offsetX>0){
+        b=1;
+        if(a!=b){
+            mirrorCat();
+        }
+        a=b;
+    }
+    console.log('offsetX:'+ offsetX + ', offsetY:'+ offsetY);
+    catx = event.clientX;
+    caty = event.clientY;
 
-}
+    var speed = 100;
+    distance = Math.sqrt(offsetX * offsetX + offsetY * offsetY);
+    var duration = distance / speed * 1000;
+
+    var cat = document.getElementById('cat');
+    // Move cat to clicked position
+    cat.style.transitionDuration = duration + "ms";
+    catPosition();
+    legsAnimation();
+    setTimeout(function() {
+        legsAnimation();
+    }, duration*0.8);
+});
 function cat(){
     const parts = ["head", "body", "tail", "leg-front-left", "leg-front-right", "leg-back-left", "leg-back-right"];
     parts.forEach(function(partId) {
@@ -24,10 +106,11 @@ function changeCat() {
     id = Math.floor(Math.random() * 6) + 1;
     cat();
 }
+var mirrored = false;
 function mirrorCat() {
     var cat = document.getElementById('cat');
     mirrored = !mirrored; // toggle
-    cat.style.transform = mirrored ? "scaleX(-1)" : "none";
+    cat.style.transform = mirrored ? "scaleX(-1) translateX(-150px)" : "none";
 }
 
 function legsAnimation() {
