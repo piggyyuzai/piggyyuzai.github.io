ea7df0b5cf2d63aa3a3265a6e413a92cac09d33b
dengdengkoi <115406234+dengdengkoi@users.noreply.github.com>
2024-05-06 01:00:28 +0800

    1

--- cat/cat.js
+++ cat/cat.js
@@ -1,13 +1,44 @@
+//红边测试
 function redBorder() {
     document.querySelectorAll('*').forEach(function(element) {
-        element.style.border = element.style.border === 'none' ? '1px solid red' : 'none';
+        element.style.border = element.style.border === '1px solid red' ? 'none' : '1px solid red';
     });
 }
 window.onload = function() {
     catpart();
     catPosition();
 }
+
+
+
+//渲染猫猫的各个部分
 id=1;
+function catpart(){
+    const parts = ["head", "body", "tail", "leg-front-left", "leg-front-right", "leg-back-left", "leg-back-right"];
+    parts.forEach(function(partId) {
+        const part = document.getElementById(partId);
+        if (partId.startsWith("leg")) { partId = "leg"; }
+        part.style.backgroundImage = "url('asset/cat/" + id + partId + ".png')";
+    });
+}
+//切换猫猫
+function changeCat() {
+    id = Math.floor(Math.random() * 6) + 1;
+    catpart();
+}
+//抚摸猫猫，播放喵喵声
+function changeHead() {
+    var meow=new Audio('audio/meow.wav')
+    meow.play();
+    head.style.backgroundImage = "url('asset/cat/"+id+"meow.png')";
+    setTimeout(function() {
+        head.style.backgroundImage = "url('asset/cat/"+id+"head.png')";
+    }, 500); // 0.5 秒后恢复原来的头部图片
+}
+
+
+
+//定位猫猫
 let catx = window.innerWidth / 2 + 0;
 let caty = window.innerHeight / 2 + 0;
 function catPosition() {
@@ -19,9 +50,13 @@ function catPosition() {
     cat.style.top = catTop + 'px';
     cat.style.left = catLeft + 'px';
 }
+//移动
 document.addEventListener('click',moveto);
 dire=1; //方向，1为右，-1为左
 function moveto(event) {
+    if (event.target.tagName.toLowerCase() !== 'body') {
+        return; // 如果点击事件的目标不是 body 元素 背景，则不执行后续操作
+    }
     document.removeEventListener('click',moveto);
     var cat = document.getElementById('cat');
     offsetX = event.clientX - catx;
@@ -50,33 +85,14 @@ function moveto(event) {
     },1);
     cat.style.transitionDuration = 0+"ms";
 }
-function catpart(){
-    const parts = ["head", "body", "tail", "leg-front-left", "leg-front-right", "leg-back-left", "leg-back-right"];
-    parts.forEach(function(partId) {
-        const part = document.getElementById(partId);
-        if (partId.startsWith("leg")) { partId = "leg"; }
-        part.style.backgroundImage = "url('asset/cat/" + id + partId + ".png')";
-    });
-}
-function changeHead() {
-    var meow=new Audio('audio/meow.wav')
-    meow.play();
-    head.style.backgroundImage = "url('asset/cat/"+id+"meow.png')";
-    setTimeout(function() {
-        head.style.backgroundImage = "url('asset/cat/"+id+"head.png')";
-    }, 500); // 0.5 秒后恢复原来的头部图片
-}
-function changeCat() {
-    id = Math.floor(Math.random() * 6) + 1;
-    catpart();
-}
+//猫猫换向
 var mirrored = false;
 function mirrorCat() {
     var cat = document.getElementById('cat');
     mirrored = !mirrored; // toggle
     cat.style.transform = mirrored ? "scaleX(-1) translateX(-150px)" : "none";
 }
-
+//四肢动作
 function legsAnimation() {
     const legs = ["leg-front-left", "leg-front-right", "leg-back-left", "leg-back-right"];
     legs.forEach(function(legId,index) {
